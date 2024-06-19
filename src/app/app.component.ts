a6825d63e3db731d579f2f546cf24355f505e08f
migreydev <miguelanreyesm@gmail.com>
2024-06-19 21:51:32 +0200

    Subiendo portafolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, NavBarComponent],
 })
 export class AppComponent {
   title = 'portfolioProject';
