193ae71509f59ae712e248890bc02a7cfca71e32
migreydev <miguelanreyesm@gmail.com>
2024-06-21 14:58:33 +0200

    modificacion navBar y añadiendo estilo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './portfolio/about/about.component';
+import { ProjectsComponent } from './portfolio/projects/projects.component';
+import { ContactComponent } from './portfolio/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+];
