e3a2620d2afe8d57cd61e6725a3e027ead2a9a25
migreydev <miguelanreyesm@gmail.com>
2024-06-28 14:33:45 +0200

    editando skills

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './portfolio/about/about.component';
 import { ProjectsComponent } from './portfolio/projects/projects.component';
 import { ContactComponent } from './portfolio/contact/contact.component';
+import { SkillsComponent } from './portfolio/skills/skills.component';
 
 export const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
 ];
